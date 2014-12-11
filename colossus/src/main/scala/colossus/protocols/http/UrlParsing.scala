4279744a100a0a0dde3bf77165ece855055f8d3d
Zoe Gagnon <zoe@tumblr.com>
2014-12-11 17:49:37 -0500

    Wild card inclusive url parsing. Uses string parsing. New operator 'on' to trigger string parsing, and '/:' to use right binding traversal of the stack, allowing the rightmost part of the pattern to consume the remainder of the string.
    Added doc comments to URL parsing

--- colossus/src/main/scala/colossus/protocols/http/UrlParsing.scala
+++ colossus/src/main/scala/colossus/protocols/http/UrlParsing.scala
@@ -1,15 +1,33 @@
 package colossus
 package protocols.http
 
-object UrlParsing {
+/**
+ * URL parsing can be done using one of two paths. The first uses an object based decomposition with a minimal number
+ * of string operations. This parses using the 'on' keyword and the left associative '/' operator.
+ *
+ * The second method uses string operations in a right associtive manner. Because all values are strings, it is less type
+ * safe than the object method. This is used with the 'in' keyword and the right associative '/:' operator. In this method,
+ * the final variable in the pattern will consume all remaining levels.
+ *
+ * To use either language, import the UrlParsing._ object
+*/
+ object UrlParsing {
+  /** Constant indicating the root of the URL for use in string based parsing */
+  val ROOT = "/"
 
+  /** Trait encapsulating parts of the parse of a url */
   sealed trait UrlComponent
+
+  /** Case class encapsulating a single piece of a parsed url */
   case class ParsedUrl(pieces: List[String]) extends UrlComponent
-  case object Root extends UrlComponent
-  case class StringUrl(url:String) extends UrlComponent
-  case object Null extends UrlComponent
+
+  /** Case object indicting the root of a url */
+  case object Root extends UrlComponent {
+    def to_s = "/"
+  }
 
   object Url {
+    /** Parses a string url into pieces for the object based url parsing DSL */
     def parse(str: String): UrlComponent = {
       val paramless = str.split("\\?").headOption
       paramless.map{ x =>
@@ -23,6 +41,9 @@ object UrlParsing {
     }
   }
 
+  /** Extractor for left-binding object based DSL. To use:
+    * case url @ Get <or other methed> on Root / first level / second level / .. repeat
+    */
   object / {
     def unapply(p: UrlComponent): Option[(UrlComponent, String)] = p match {
       case ParsedUrl(items) if (items.size == 1) => Some((Root, items.head))
@@ -31,6 +52,22 @@ object UrlParsing {
     }
   }
 
+  /** Extractor for right-binding string based DSL. To use:
+    * case irl @ Get <or other method> in ROOT /: first level /: second level /: remainder
+    */
+  object /: {
+    def unapply(s:String):Option[(String, String)] = s match {
+      case url:String if (url == "/") => Some((url, ""))
+      case url:String if (url startsWith "/") => Some("/", url drop 1)
+      case url:String  =>
+        val last = url.indexOf('/')
+        val tail = url.substring(last + 1)
+        val head = url.substring(0, last)
+        Some((if (head.isEmpty) "/" else head, tail))
+    }
+  }
+
+  /** Extractor for integers */
   object Integer {
     def unapply(s: String) : Option[Int] = try {
       Some(s.toInt)
@@ -38,12 +75,24 @@ object UrlParsing {
       case n: java.lang.NumberFormatException => None
     }
   }
-  
+
+  /**
+   * Keyword which triggers left-binding object based parsing. Must be used to match a url pattern with an HTTP method.
+   */
   case object on {
     def unapply(request: HttpRequest): Option[(HttpMethod, UrlComponent)] = Some(request.head.method, Url.parse(request.head.url))
   }
 
+  /**
+   * Keyword which triggers right-binding string based parsing. Must be used to match a url pattern with an HTTP method.
+   */
   case object in {
-    def unapply(request:HttpRequest):Option[(HttpMethod, UrlComponent)] = Some(request.head.method, Root)
+    def unapply(request: HttpRequest): Option[(HttpMethod, String)] = {
+      val component = request.head.url match {
+        case "" => "/"
+        case url:String => url
+      }
+      Some(request.head.method, component)
+    }
   }
 }
--- colossus/src/test/scala/WildcardURLParsingTest.scala
+++ colossus/src/test/scala/WildcardURLParsingTest.scala
@@ -10,12 +10,43 @@ class WildcardURLParsingTest extends WordSpec with MustMatchers {
   import colossus.protocols.http.UrlParsing._
     "The wildcard url parser" should {
       "match a root url" in {
-          val url = """http://test.com"""
+          val url = ""
           val request = HttpRequest(HttpHead(Get, url, `1.1`, List()), None)
           request match {
-            case req @ Get in Root => true
+            case req @ Get in ROOT => true
             case _ => fail()
           }
+      }
+
+      "match a resource url" in {
+        val url = "/moose.jpg"
+        val request = HttpRequest(HttpHead(Get, url, `1.1`, List()), None)
+        request match {
+          case req @ Get in ROOT /: rest => rest must be("moose.jpg")
+          case _ => fail("Failed to match url")
+        }
+      }
+
+      "match a complex resource url" in {
+        val url = "/bronx/zoo/moose.jpg"
+        val request = HttpRequest(HttpHead(Get, url, `1.1`, List()), None)
+        request match {
+          case req @ Get in base /: city /: place /: animal  =>
+            base must be("/")
+            city must be("bronx")
+            place must be("zoo")
+            animal must be("moose.jpg")
+          case _ => fail("Failed to match complex url")
+        }
+      }
+
+      "partially match a url" in {
+        val url = "/bronx/zoo/moose.jpg"
+        val request = HttpRequest(HttpHead(Get, url, `1.1`, List()), None)
+        request match {
+          case req @ Get in ROOT /: city /: placeAndAnimal => placeAndAnimal must be("zoo/moose.jpg")
+          case _ => fail("Failed to match complex url")
+        }
       }
     }
 }
