7f5783c52fb16ea867a4a7c557f3970c0048efad
Dan Simon <dan@tumblr.com>
2015-01-06 17:37:43 -0500

    adding support for http chunked transfer

--- colossus/src/main/scala/colossus/protocols/http/HttpParse.scala
+++ colossus/src/main/scala/colossus/protocols/http/HttpParse.scala
@@ -1,7 +1,7 @@
 package colossus
 package protocols.http
 
-import akka.util.ByteString
+import akka.util.{ByteString, ByteStringBuilder}
 
 import parsing._
 import Combinators._
@@ -16,6 +16,13 @@ object HttpParse {
     stringUntil(':', toLower = true, allowWhiteSpace = false) ~ 
     stringUntil('\r', allowWhiteSpace = true, ltrim = true) <~ 
     byte >> {case key ~ value => (key, value)}
+  }
+
+  def chunkedBody: Parser[ByteString] = chunkedBodyBuilder(new ByteStringBuilder)
+
+  private def chunkedBodyBuilder(builder: ByteStringBuilder): Parser[ByteString] = intUntil('\r', 16) <~ byte |> {
+    case 0 => bytes(2) >> {_ => builder.result}
+    case n => bytes(n.toInt) <~ bytes(2) |> {bytes => chunkedBodyBuilder(builder.append(bytes))}
   }
 
 }
