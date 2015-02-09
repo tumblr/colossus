515c8eb637efdace3a0c5d49ee7cfc35c6fceb98
Dan Simon <dan@tumblr.com>
2015-02-09 16:34:32 -0500

    Milestone Release: 0.6.0-M2

--- project/Build.scala
+++ project/Build.scala
@@ -16,7 +16,7 @@ object ColossusBuild extends Build {
     organization := "com.tumblr",
     scalaVersion  := "2.11.4",
     crossScalaVersions := Seq("2.10.4", "2.11.2"),
-    version                   := "0.6.0-SNAPSHOT",
+    version                   := "0.6.0-M2",
     parallelExecution in Test := false,
     scalacOptions <<= scalaVersion map { v: String =>
       val default = List(
