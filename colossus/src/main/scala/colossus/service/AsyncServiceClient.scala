e742997876cdeb10f5da57e3d9e50459fd4402d5
Dan Simon <dan@tumblr.com>
2015-02-17 18:21:26 -0500

    removing shared interface from ServiceClient, LoadbalancingClient

--- colossus/src/main/scala/colossus/service/AsyncServiceClient.scala
+++ colossus/src/main/scala/colossus/service/AsyncServiceClient.scala
@@ -63,7 +63,10 @@ class ClientProxy(config: ClientConfig, system: IOSystem, handlerFactory: ActorR
 
 }
 
-trait AsyncServiceClient[I,O] extends SharedClient[I,O] {
+trait AsyncServiceClient[I,O]  {
+  
+  def send(request: I): Future[O]
+
   def connectionStatus: Future[ConnectionStatus]
   def disconnect()
 
