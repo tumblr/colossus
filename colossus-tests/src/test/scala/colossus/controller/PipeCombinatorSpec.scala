a8743ab26e1dadb49f8f1274c66f13d58b29c8f8
Dan Simon <dan@tumblr.com>
2015-02-16 19:22:23 -0500

    all tests pass

--- colossus-tests/src/test/scala/colossus/controller/PipeCombinatorSpec.scala
+++ colossus-tests/src/test/scala/colossus/controller/PipeCombinatorSpec.scala
@@ -7,21 +7,15 @@ import scala.util.{Failure, Success}
 class PipeCombinatorSpec extends WordSpec with MustMatchers with OptionValues with TryValues{
 
   "PipeCombinator" must {
-  /*
 
     "combine 2 pipes with a map function" in {
 
-      import ReflexiveCopiers._
-
-      val writeSink: Pipe[String, String] = new InfinitePipe[String](100)
-      val readSource: Pipe[Int, Int] = new InfinitePipe[Int](100)
+      val writeSink: Pipe[String, String] = new InfinitePipe[String]
+      val readSource: Pipe[Int, Int] = new InfinitePipe[Int]
 
       val map: (String) => Seq[Int] = (s: String) => s.split(":").map(_.toInt).toSeq
 
       val combined: Pipe[String, Int] = writeSink.join(readSource)(map)
-      combined.push("1:2:3")
-      combined.push("4:5")
-      combined.complete()
 
       var callbackExecuted = false
 
@@ -29,24 +23,23 @@ class PipeCombinatorSpec extends WordSpec with MustMatchers with OptionValues wi
         case Success(x) => callbackExecuted = true; x must equal(Seq(1, 2, 3, 4, 5))
         case Failure(t) => throw t
       }
+      callbackExecuted must be (false)
+      combined.push("1:2:3") must equal(PushResult.Ok)
+      combined.push("4:5") must equal(PushResult.Ok)
+      combined.complete()
 
       callbackExecuted must be (true)
     }
 
     "completing a pipe will complete both sides" in {
-      import ReflexiveCopiers._
-
-      val writeSink: Pipe[String, String] = new InfinitePipe[String](100)
-      val readSource: Pipe[Int, Int] = new InfinitePipe[Int](100)
-
+      val writeSink: Pipe[String, String] = new InfinitePipe[String]
+      val readSource: Pipe[Int, Int] = new InfinitePipe[Int]
       val map: (String) => Seq[Int] = (s: String) => s.split(":").map(_.toInt).toSeq
-
       val combined: Pipe[String, Int] = writeSink.join(readSource)(map)
 
       combined.complete()
 
-      val t = writeSink.push("")
-      t.failure.exception mustBe a [PipeClosedException]
+      writeSink.push("") must equal(PushResult.Closed)
 
       readSource.pull {
         case Success(None) => {}
@@ -58,10 +51,8 @@ class PipeCombinatorSpec extends WordSpec with MustMatchers with OptionValues wi
 
       class GadZooksException(msg : String) extends Exception(msg)
 
-      import ReflexiveCopiers._
-
-      val writeSink: Pipe[String, String] = new InfinitePipe[String](100)
-      val readSource: Pipe[Int, Int] = new InfinitePipe[Int](100)
+      val writeSink: Pipe[String, String] = new InfinitePipe[String]
+      val readSource: Pipe[Int, Int] = new InfinitePipe[Int]
 
       val map: (String) => Seq[Int] = (s: String) => s.split(":").map(_.toInt).toSeq
 
@@ -70,19 +61,16 @@ class PipeCombinatorSpec extends WordSpec with MustMatchers with OptionValues wi
       combined.terminate(new GadZooksException("snip snap"))
 
       val t = writeSink.push("")
-      t.failure.exception mustBe a [PipeTerminatedException]
-      val ex = t.failure.exception.asInstanceOf[PipeTerminatedException]
-      ex.getCause mustBe a [GadZooksException]
+      t mustBe a[PushResult.Error]
 
 
       readSource.pull {
         case Failure(ex : PipeTerminatedException) => {
-          ex.getCause mustBe a [GadZooksException]
+          ex.getCause mustBe a[GadZooksException]
         }
-        case _ => throw new Exception("expected None")
+        case other => throw new Exception(s"expected None, got $other")
       }
     }
-    */
   }
 
 }
