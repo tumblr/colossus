2450c082f2985ab4a6915cc55a47df9c686fddbf
Zoe Gagnon <zoe@tumblr.com>
2015-02-20 17:28:05 -0500

    Document some of the API for metrics

--- colossus-metrics/src/main/scala/colossus/metrics/MetricSystem.scala
+++ colossus-metrics/src/main/scala/colossus/metrics/MetricSystem.scala
@@ -2,17 +2,28 @@ package colossus.metrics
 
 import akka.actor._
 import akka.agent.Agent
-import akka.pattern.ask
+import colossus.metrics.MetricAddress.Root
 
-import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.duration._
-import akka.util.Timeout
-import MetricAddress.Root
 
 
 case class MetricSystemId(id: Long)
 
-
+/**
+ * The MetricSystem is a set of actors which handle the background operations of dealing with metrics. In most cases,
+ * you only want to thave one MetricSystem per application.
+ *
+ * Metrics are generated periodically by a Tick message published on the global event bus. By default this happens once
+ * per second, but it can be configured to any time interval. So while events are being collected as they occur,
+ * compiled metrics (such as rates and histogram percentiles) are generated once per tick.
+ *
+ * @param id The ID of the metrics system
+ * @param namespace the base of the url describing the location of metrics within the system
+ * @param clock an actor which serves as the clock for the metric system
+ * @param database an actor which serves as the database of the metric system
+ * @param snapshot
+ * @param tickPeriod The frequency of the tick message
+ */
 case class MetricSystem(id: MetricSystemId, namespace: MetricAddress, clock: ActorRef, database: ActorRef, snapshot: Agent[MetricMap], tickPeriod: FiniteDuration) {
 
 
@@ -20,6 +31,12 @@ case class MetricSystem(id: MetricSystemId, namespace: MetricAddress, clock: Act
 
   def query(queryString: String): MetricMap = query(MetricFilter(queryString))
 
+  /**
+   * Configures the reporting of the metric system.
+   * @param config The [[MetricReporterConfig]] to use when configuring reporting
+   * @param fact
+   * @return
+   */
   def report(config: MetricReporterConfig)(implicit fact: ActorRefFactory): ActorRef = MetricReporter(config)(this, fact)
 
   def sharedCollection(globalTags: TagMap = TagMap.Empty)(implicit fact: ActorRefFactory) = {
@@ -32,6 +49,14 @@ case class MetricSystem(id: MetricSystemId, namespace: MetricAddress, clock: Act
 }
 
 object MetricSystem {
+  /**
+   * Constructs a metric system
+   * @param namespace the base of the url describing the location of metrics within the system
+   * @param tickPeriod The frequency of the tick message
+   * @param collectSystemMetrics whether to collect metrics from the system as well
+   * @param system the actor system the metric system should use
+   * @return
+   */
   def apply(namespace: MetricAddress, tickPeriod: FiniteDuration = 1.second, collectSystemMetrics: Boolean = true)
   (implicit system: ActorSystem): MetricSystem = {
     import system.dispatcher
@@ -46,7 +71,7 @@ object MetricSystem {
   }
 
   def deadSystem(implicit system: ActorSystem) = {
-    import ExecutionContext.Implicits.global //this is ok since we're using it to create an agent that's never used
+    import scala.concurrent.ExecutionContext.Implicits.global //this is ok since we're using it to create an agent that's never used
     MetricSystem(MetricSystemId(System.nanoTime), Root / "DEAD", system.deadLetters, system.deadLetters, Agent[MetricMap](Map()), 0.seconds)
   }
 
