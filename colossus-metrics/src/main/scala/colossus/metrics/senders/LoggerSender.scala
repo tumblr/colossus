71f6bd44eaec679c00d35a29af18c1297caa17fd
Zoe Gagnon <zoe@tumblr.com>
2015-02-26 17:04:27 -0500

    Add the ability to change the format in the logger metric sender

--- colossus-metrics/src/main/scala/colossus/metrics/senders/LoggerSender.scala
+++ colossus-metrics/src/main/scala/colossus/metrics/senders/LoggerSender.scala
@@ -2,11 +2,12 @@ package colossus.metrics
 
 import akka.actor._
 import colossus.metrics.senders.MetricsLogger
+import colossus.metrics.senders.MetricsLogger.Formatter
 
 /**
  * Simple sender that just prints the stats to the log
  */
-class LoggerSenderActor extends Actor with ActorLogging with  MetricsLogger {
+class LoggerSenderActor(override val formatter:Formatter) extends Actor with ActorLogging with  MetricsLogger {
 
   def receive = {
     case s: MetricSender.Send => {
@@ -22,11 +23,18 @@ class LoggerSenderActor extends Actor with ActorLogging with  MetricsLogger {
   override def postStop() {
     log.info("shutting down dummy sender")
   }
+}
 
+class LoggerSender(val formatter:Formatter) extends MetricSender {
+  override def name: String = "logger"
+
+  override def props: Props = Props(classOf[LoggerSenderActor], formatter)
 }
 
 object LoggerSender extends MetricSender {
   def name = "logger"
-  def props = Props(classOf[LoggerSenderActor])
+  def props = Props(classOf[LoggerSenderActor], MetricsLogger.defaultMetricsFormatter)
+
+  def apply(formatter:Formatter) = new LoggerSender(formatter)
 }
 
