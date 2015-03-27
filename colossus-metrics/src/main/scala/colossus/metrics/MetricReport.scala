1110c306f5e6883ad38c8f5252861753dc1fcfc6
Dan Simon <dan@tumblr.com>
2015-03-27 11:20:19 -0400

    separeted metrics from raw metrics

--- colossus-metrics/src/main/scala/colossus/metrics/MetricReport.scala
+++ colossus-metrics/src/main/scala/colossus/metrics/MetricReport.scala
@@ -1,4 +1,4 @@
 package colossus.metrics
 
 
-case class MetricReport(globalTags: TagMap, metrics: MetricMap.SerializedMetricMap, reportingFreqMillis: Int, timestamp: Long)
+case class MetricReport(globalTags: TagMap, metrics: RawMetricMap.SerializedMetricMap, reportingFreqMillis: Int, timestamp: Long)
