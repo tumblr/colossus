8edae6fd9d6bd5e02b7432e8dc8cefca870f8c6e
Nick sauro <sauron@tumblr.com>
2015-04-06 15:56:29 -0400

    resetting version to M1

--- project/Build.scala
+++ project/Build.scala
@@ -16,7 +16,7 @@ object ColossusBuild extends Build {
     organization := "com.tumblr",
     scalaVersion  := "2.11.5",
     crossScalaVersions := Seq("2.10.4", "2.11.5"),
-    version                   := "0.6.1-RC1",
+    version                   := "0.6.1-M1",
     parallelExecution in Test := false,
     scalacOptions <<= scalaVersion map { v: String =>
       val default = List(
