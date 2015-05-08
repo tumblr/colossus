f109897cb686c44963eb9d7e244e18b964cdaf92
Nick sauro <sauron@tumblr.com>
2015-05-08 11:29:15 -0400

    added negative Callback matcher

--- colossus-testkit/src/test/scala/colossus/CallbackMatchersSpec.scala
+++ colossus-testkit/src/test/scala/colossus/CallbackMatchersSpec.scala
@@ -75,4 +75,54 @@ class CallbackMatchersSpec extends WordSpec with MustMatchers{
 
   }
 
+  "A CallbackFailTo matcher" should {
+    "fail to match if a Callback never executes" in {
+      def cbFunc(f : Try[Int] => Unit) {
+
+      }
+
+      val unmapped = UnmappedCallback(cbFunc)
+
+      var execd = false
+      val result = new CallbackFailTo[Int](a => execd = true).apply(unmapped)
+      result.matches must equal(false)
+      execd must equal(false)
+    }
+
+    "fail to match if the 'evaluate' function throws" in {
+      var execd = false
+      val cb = Callback.failed(new Exception("D'OH!"))
+      val eval = (a : Throwable) => {
+        execd = true
+        a must equal("awesome!")
+      }
+      val result = new CallbackFailTo[String](eval).apply(cb)
+      result.matches must equal(false)
+      execd must equal(true)
+    }
+
+    "fail to match if the Callback executes successfully" in {
+      var execd = false
+      val cb = Callback.successful("success!")
+      val eval = (a : Throwable) => {
+        execd = true
+        a must not be null
+      }
+      val result = new CallbackFailTo[String](eval).apply(cb)
+      result.matches must equal(false)
+      execd must equal(false)
+    }
+
+    "match if a Callback fails" in {
+      var execd = false
+      val cb = Callback.failed(new Exception("D'OH!"))
+      val eval = (a : Throwable) => {
+        execd = true
+      }
+      val result = new CallbackFailTo[String](eval).apply(cb)
+      result.matches must equal(true)
+      execd must equal(true)
+    }
+  }
+
 }
