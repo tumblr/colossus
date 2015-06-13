853faf8fc05c0e92b33308e38cd09817ae72f9f6
Dan Simon <dan@tumblr.com>
2015-06-13 12:20:36 -0400

    cleaning up examples

--- colossus-examples/src/main/scala/colossus-examples/Main.scala
+++ colossus-examples/src/main/scala/colossus-examples/Main.scala
@@ -19,10 +19,10 @@ object Main extends App {
 
   implicit val actorSystem = ActorSystem("COLOSSUS")
 
-  implicit val ioSystem = IOSystem()//"examples", numWorkers = Some(1))
+  implicit val ioSystem = IOSystem()
 
   //the simplest example, an echo server over telnet
-  //val telnetServer = TelnetExample.start(9000)
+  val telnetServer = TelnetExample.start(9000)
 
   //http service which communicates with a key/value store over the redis protocol
   val httpServer = HttpExample.start(9001, new InetSocketAddress("localhost", 9002))
@@ -31,80 +31,7 @@ object Main extends App {
   val keyvalServer = KeyValExample.start(9002)
 
   //an echo server built only on the core layer
-  //val echoServer = EchoExample.start(9003)
+  val echoServer = EchoExample.start(9003)
 
   //chat server using the controller layer
-  //val chatServer = ChatExample.start(9005)
-import java.util.Date
-import java.text.SimpleDateFormat
-
-import colossus._
-import colossus.core.ServerRef
-import service._
-import protocols.http._
-
-import net.liftweb.json._
-import JsonDSL._
-import akka.actor._
-import akka.util.ByteString
-import scala.concurrent.duration._
-
-class Timestamp(server: ServerRef) extends Actor {
-      
-  val sdf = new SimpleDateFormat("EEE, MMM d yyyy HH:MM:ss z")
-  case object Tick
-  import context.dispatcher
-
-  override def preStart() {
-    self ! Tick
-  }
-
-  def receive = {
-    case Tick => {
-      server.delegatorBroadcast(sdf.format(new Date()))
-      context.system.scheduler.scheduleOnce(1.second, self, Tick)
-    }
-  }
-}
-  val response          = "Hello, World!"
-  val plaintextHeader   = ("Content-Type", "text/plain")
-  val jsonHeader        = ("Content-Type", "application/json")
-  val serverHeader      = ("Server", "Colossus")
-
-  val server = Service.serve[Http]("sample", 9007) { context =>
-
-    var dateHeader = ("Date", "???")
-
-    context.receive {
-      case ts: String => dateHeader = ("Date", ts)
-    }
-    
-    context.handle { connection =>
-      connection.become{ case request =>
-        if (request.head.url == "/plaintext") {
-          val res = HttpResponse(
-            version  = HttpVersion.`1.1`,
-            code    = HttpCodes.OK,
-            data    = response,
-            headers = Vector(plaintextHeader, serverHeader, dateHeader)
-          )
-          Callback.successful(res)
-        } else if (request.head.url == "/json") {
-          val json = ("message" -> "Hello, World!")
-          val res = HttpResponse(
-            version  = HttpVersion.`1.1`,
-            code    = HttpCodes.OK,
-            data    = compact(render(json)),
-            headers = Vector(jsonHeader, serverHeader, dateHeader)
-          )
-          Callback.successful(res)
-        } else {
-          Callback.successful(request.notFound("invalid path"))
-        }
-      }
-    }
-  }
-
-  val timestamp = ioSystem.actorSystem.actorOf(Props(classOf[Timestamp], server))
-
-}
+  val chatServer = ChatExample.start(9005)
