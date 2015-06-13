723a203b7a6b7d501def68f7bb208ac4adb5d6c0
Dan Simon <dan@tumblr.com>
2015-06-12 22:06:05 -0400

    Removing broken Executor, new TestExecutor (that works)

--- colossus-testkit/src/main/scala/colossus-testkit/FakeIOSystem.scala
+++ colossus-testkit/src/main/scala/colossus-testkit/FakeIOSystem.scala
@@ -9,7 +9,8 @@ import akka.actor._
 import akka.testkit.TestProbe
 import akka.testkit.CallingThreadDispatcher
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{Await, ExecutionContext, Future, Promise}
+import scala.concurrent.duration._
 
 object FakeIOSystem {
   def apply()(implicit system: ActorSystem): IOSystem = {
@@ -29,19 +30,52 @@ object FakeIOSystem {
     (sys, probe)
   }
 
-  /**
-   * Returns an entirely in-thread CallbackExecutor
-   */
-  def callingThreadCallbackExecutor(implicit system: ActorSystem): CallbackExecutor = {
-    class FakeExecutor extends Actor with CallbackExecution {
-      def receive = handleCallback
+  //prevents accidentally spinning up a new executor per test
+  private val exCache: collection.mutable.Map[ActorSystem, CallbackExecutor] = collection.mutable.Map()
+
+  def testExecutor(implicit system: ActorSystem): CallbackExecutor = {
+    exCache.get(system).getOrElse {
+      val ref = system.actorOf(Props[GenericExecutor].withDispatcher("server-dispatcher"))
+      val ex = CallbackExecutor(system.dispatcher, ref)
+      exCache(system) = ex
+      ex
     }
-    val ref = system.actorOf(Props[FakeExecutor].withDispatcher(CallingThreadDispatcher.Id))
-    val executor = new ExecutionContext {
-      def reportFailure(t: Throwable) { t.printStackTrace() }
-      def execute(runnable: Runnable) {runnable.run()}
-    }
-    CallbackExecutor(executor, ref)
   }
 }
 
+
+trait GenericCallback {
+  def execute()
+}
+
+case class CallbackMessage[T](cb: Callback[T]) extends GenericCallback {
+  def execute() {
+    cb.execute()
+  }
+}
+
+class GenericExecutor extends Actor with CallbackExecution {
+  def receive = handleCallback orElse {
+    case g: GenericCallback => {
+      g.execute()
+    }
+  }
+}
+
+
+object CallbackAwait {
+
+  /**
+   * Await the result of a Callback.  This *must* be used whenever Callback.fromFuture is used.  Going forward this may be the only way to
+   * extract the value from a Callback
+   *
+   * The Callback is properly executed inside an Actor running in a PinnedDispatcher.  The calling thread blocks until the Callback finishes
+   * execution or the timeout is reached
+   */
+  def result[T](cb: Callback[T], in: Duration)(implicit ex: CallbackExecutor): T = {
+    val p = Promise[T]()
+    ex.executor ! CallbackMessage(cb.mapTry{t => p.complete(t);t})
+    Await.result(p.future, in)
+  }
+
+}
