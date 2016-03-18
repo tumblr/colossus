941a7719d5770c8381fa82a7ede9efa4c2e05288
Nick sauro <sauron@tumblr.com>
2016-03-18 09:12:54 -0400

    fixed broken test

--- colossus-metrics/src/test/scala/colossus/metrics/MetricSpec.scala
+++ colossus-metrics/src/test/scala/colossus/metrics/MetricSpec.scala
@@ -24,8 +24,8 @@ class MetricSpec(_system : ActorSystem) extends MetricIntegrationSpec(_system) w
 
   "MetricSystem" must {
     "allow multiple systems to start without any conflicts" in {
-      val m1 = MetricSystem("/sys1")
-      val m2 = MetricSystem("/sys2")
+      val m1 = MetricSystem(MetricAddress("/sys1"))
+      val m2 = MetricSystem(MetricAddress("/sys2"))
       //no exceptions means the test passed
     }
 
@@ -43,7 +43,7 @@ class MetricSpec(_system : ActorSystem) extends MetricIntegrationSpec(_system) w
         )
       )
       val expected = parse(
-        """{"/foo" : [ 
+        """{"/foo" : [
           {"tags" : {"a" : "va"}, "value" : 3},
           {"tags" : {"b" : "vb"}, "value" : 4}
           ]}"""
@@ -61,7 +61,7 @@ class MetricSpec(_system : ActorSystem) extends MetricIntegrationSpec(_system) w
         )
       )
       val json = parse(
-        """{"/foo" : [ 
+        """{"/foo" : [
           {"tags" : {"a" : "va"}, "value" : 3},
           {"tags" : {"b" : "vb"}, "value" : 4}
           ]}"""
