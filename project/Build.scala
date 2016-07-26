e36d4ad16a1548400cfb5ef0495beeff33127baf
Dan Simon <dan@tumblr.com>
2016-07-26 17:57:31 -0400

    candidate release 0.8.1-RC3

--- project/Build.scala
+++ project/Build.scala
@@ -19,7 +19,7 @@ object ColossusBuild extends Build {
     organization := "com.tumblr",
     scalaVersion  := "2.11.7",
     crossScalaVersions := Seq("2.10.6", "2.11.7"),
-    version                   := "0.8.1-SNAPSHOT",
+    version                   := "0.8.1-RC3",
     parallelExecution in Test := false,
     scalacOptions <<= scalaVersion map { v: String =>
       val default = List(
