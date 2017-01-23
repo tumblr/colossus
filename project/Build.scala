ea128cf144c3b2b47619cc6f8164b165eec377fe
Dan Simon <dan@tumblr.com>
2017-01-23 13:42:49 -0500

    back to snapshot

--- project/Build.scala
+++ project/Build.scala
@@ -19,7 +19,7 @@ object ColossusBuild extends Build {
     organization := "com.tumblr",
     scalaVersion  := "2.11.8",
     crossScalaVersions := Seq("2.10.6", "2.11.8"),
-    version                   := "0.9.0-RC1",
+    version                   := "0.9.0-SNAPSHOT",
     parallelExecution in Test := false,
     scalacOptions <<= scalaVersion map { v: String =>
       val default = List(
