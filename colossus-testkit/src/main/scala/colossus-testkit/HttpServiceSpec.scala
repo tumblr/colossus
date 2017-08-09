2be9918abbf15449291b0bfc11724c8a77efa4a9
David <dxuhuang@gmail.com>
2017-08-09 16:45:31 -0400

    add a test that applies a predicate to response body (#524)

--- colossus-testkit/src/main/scala/colossus-testkit/HttpServiceSpec.scala
+++ colossus-testkit/src/main/scala/colossus-testkit/HttpServiceSpec.scala
@@ -8,21 +8,30 @@ import protocols.http._
 abstract class HttpServiceSpec extends ServiceSpec[Http] {
 
   def expectCode(request: HttpRequest, expectedResponseCode: HttpCode) {
-    withClient{client =>
+    withClient { client =>
       val resp = Await.result(client.send(request), requestTimeout)
       assertCode(request, resp, expectedResponseCode)
     }
   }
 
-  def expectCodeAndBody(request: HttpRequest, expectedResponseCode: HttpCode, body : String) {
-    withClient{client =>
+  def expectCodeAndBody(request: HttpRequest, expectedResponseCode: HttpCode, expectedBody: String) {
+    withClient { client =>
       val resp = Await.result(client.send(request), requestTimeout)
       assertCode(request, resp, expectedResponseCode)
-      assertBody(request, resp, body)
+      assertBody(request, resp, expectedBody)
     }
   }
 
-  def assertCode(request : HttpRequest, response : HttpResponse, expectedCode : HttpCode) {
+  def expectCodeAndBodyPredicate(request: HttpRequest, expectedResponseCode: HttpCode, bodyPredicate: String => Boolean) = {
+    withClient { client =>
+      val response = Await.result(client.send(request), requestTimeout)
+      assertCode(request, response, expectedResponseCode)
+      val body = response.body.bytes.utf8String
+      assert(bodyPredicate(body))
+    }
+  }
+
+  def assertCode(request: HttpRequest, response: HttpResponse, expectedCode: HttpCode) {
     val msg = request.head.method +
       " " + request.head.url + ": Code " +
       response.head.code + " did not match " +
@@ -31,8 +40,7 @@ abstract class HttpServiceSpec extends ServiceSpec[Http] {
     assert(response.head.code == expectedCode, msg)
   }
 
-
-  def assertBody(request : HttpRequest, response : HttpResponse, expectedBody : String){
+  def assertBody(request: HttpRequest, response: HttpResponse, expectedBody: String){
     val body = response.body.bytes.utf8String
     assert(body == expectedBody, s"$body did not equal $expectedBody")
   }
