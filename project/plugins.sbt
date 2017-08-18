1bc09acb2c3eb14df0d17572208b9c66f9fe3ede
Ali Yakamercan <yaka@tumblr.com>
2017-08-18 11:33:56 -0400

    Minor changes to publish

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -4,7 +4,9 @@ addSbtPlugin("io.spray" % "sbt-revolver" % "0.8.0")
 
 addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "0.6.0")
 
-addSbtPlugin("com.typesafe.sbt" % "sbt-pgp" % "0.8.3")
+addSbtPlugin("com.jsuereth" % "sbt-pgp" % "1.0.0")
+
+addSbtPlugin("org.xerial.sbt" % "sbt-sonatype" % "1.1")
 
 addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.5.0")
 
---
+++ publish.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if [[ CI != 'true' ]]; then
+    echo "Not running on Travis-ci"
+    exit 0
+fi
+ 
+if [[ $TRAVIS_BRANCH =~ ^v[0-9]+\.[0-9]+\.[0-9]+(-RC[0-9]+)?$ || ($TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "develop" && $(cat version.sbt) =~ "-SNAPSHOT") ]]; then
+    # decrypt secrets & untar 
+    openssl aes-256-cbc -K $encrypted_9df8dc1a95a3_key -iv $encrypted_9df8dc1a95a3_iv -in secrets.tar.enc -out secrets.tar -d
+    tar xvf secrets.tar
+    # publish, close & release
+    sbt publishSigned sonatypeReleaseAll
+else 
+    echo 'Only publishing on version tags or develop snapshots'
+    exit 0
+fi
