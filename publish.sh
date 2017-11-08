72941b79105f64944ee8648ee24db1dd1c8978a3
Ali Yakamercan <yaka@tumblr.com>
2017-11-08 14:25:13 -0500

    let sbt handle cross building

--- publish.sh
+++ publish.sh
@@ -10,7 +10,7 @@ if [[ $TRAVIS_BRANCH =~ ^v[0-9]+\.[0-9]+\.[0-9]+(-RC[0-9]+|-M[0-9]+)?$ || ($TRAV
     openssl aes-256-cbc -K $encrypted_9df8dc1a95a3_key -iv $encrypted_9df8dc1a95a3_iv -in secrets.tar.enc -out secrets.tar -d
     tar xvf secrets.tar
     # publish, close & release
-    sbt ++$TRAVIS_SCALA_VERSION publishSigned sonatypeReleaseAll
+    sbt +publishSigned +sonatypeReleaseAll
 else 
     echo 'Only publishing on version tags or develop snapshots'
     exit 0
