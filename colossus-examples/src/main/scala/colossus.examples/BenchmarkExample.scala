92aed36cbb451730a4234b9abcffe0c6fcd39eb8
Dave Karivalis <dkarivalis@gmail.com>
2017-12-15 12:51:23 -0500

    rename encoder; update plaintext route

--- colossus-examples/src/main/scala/colossus.examples/BenchmarkExample.scala
+++ colossus-examples/src/main/scala/colossus.examples/BenchmarkExample.scala
@@ -10,17 +10,16 @@ object BenchmarkService {
 
   private val mapper: ObjectMapper = new ObjectMapper().registerModule(DefaultScalaModule)
 
-  implicit object JsonEncoder extends HttpBodyEncoder[Array[Byte]] {
+  implicit object JsonBody extends HttpBodyEncoder[Array[Byte]] {
     val contentType = Some(ContentType.ApplicationJson)
     def encode(json: Array[Byte]): HttpBody = new HttpBody(json)
   }
 
   def json: Array[Byte] = mapper.writeValueAsBytes(Map("message" -> "Hello, World!"))
-  val plaintext    = HttpBody("Hello, World!")
 
   def start(port: Int)(implicit io: IOSystem) {
     HttpServer.basic("benchmark", port) {
-      case req if req.head.url == "/plaintext" => req.ok(plaintext)
+      case req if req.head.url == "/plaintext" => req.ok("Hello, World!")
       case req if req.head.url == "/json"      => req.ok(json)
     }
   }
