d5d80bba007018bb1b5c039c17882d2c07b5ced7
dave karivalis <dkarivalis@gmail.com>
2018-01-08 14:29:14 -0500

    Swap benchmark example json lib (#648)
    swap benchmark example json lib

--- colossus-examples/src/main/scala/colossus.examples/BenchmarkExample.scala
+++ colossus-examples/src/main/scala/colossus.examples/BenchmarkExample.scala
@@ -3,29 +3,25 @@ package colossus.examples
 import colossus.core.IOSystem
 import colossus.service.Callback.Implicits._
 import colossus.protocols.http._
-import org.json4s._
-import org.json4s.jackson.JsonMethods._
-import org.json4s.JsonDSL._
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
 
-object BenchmarkService {
 
-  implicit object JsonBody extends HttpBodyEncoder[JValue] {
-    val contentType = ContentType.ApplicationJson
+object BenchmarkExample {
 
-    def encode(json: JValue) = {
-      HttpBody(compact(render(json)))
-    }
+  implicit object JsonBody extends HttpBodyEncoder[AnyRef] {
+    private val mapper: ObjectMapper = new ObjectMapper().registerModule(DefaultScalaModule)
+    val contentType: String = ContentType.ApplicationJson
+    def encode(helloWorld: AnyRef): HttpBody = new HttpBody(mapper.writeValueAsBytes(helloWorld))
   }
 
-  val json: JValue = ("message" -> "Hello, World!")
+  case class HelloWorld(message: String)
+  def helloWorld: AnyRef = HelloWorld("Hello, World!")
 
   def start(port: Int)(implicit io: IOSystem) {
-
     HttpServer.basic("benchmark", port) {
-      case req if (req.head.url == "/plaintext") => req.ok("Hello, World!")
-      case req if (req.head.url == "/json")      => req.ok(json)
+      case req if req.head.url == "/plaintext" => req.ok("Hello, World!")
+      case req if req.head.url == "/json"      => req.ok(helloWorld)
     }
-
   }
-
 }
